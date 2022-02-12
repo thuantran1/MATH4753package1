3024c5e2a76fc679261ad7bed1ae8908133c9c0c
Thuan <thuan.m.tran-1@ou.edu>
2022-02-11 20:35:30 -0600

    Initialize

---
+++ R/package1.R
@@ -0,0 +1,10 @@
+#' Labs Package
+#'
+#' Contains various functions from previous labs for newer labs where using this package is necesssary.
+#'
+#' @doctype package
+#'
+#' @author Thuan Tran <thuan.m.tran-1@ou.edu>
+#'
+#' @name package1
+NULL
